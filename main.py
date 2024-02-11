da830018ed534be40d6ecb33169faf3e687bc587
Manipradeep Marichamy <40050620+mmanipradeep@users.noreply.github.com>
2024-02-11 22:17:13 +0530

    updated REST API

--- main.py
+++ main.py
@@ -1,119 +1,105 @@
-from fastapi import FastAPI, status, HTTPException
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from pydantic import BaseModel
-from sqlalchemy.orm import Session
-
-# Create a sqlite engine instance
-engine = create_engine("sqlite:///todooo.db")
-
-# Create a DeclarativeMeta instance
-Base = declarative_base()
-
-# Define To Do class inheriting from Base
-class ToDo(Base):
-    __tablename__ = 'todos'
-    id = Column(Integer, primary_key=True)
-    task = Column(String(256))
-
+import contextlib
+from fastapi import FastAPI, HTTPException, Query
+from pymongo import MongoClient
+from bson import ObjectId
+from fastapi.encoders import jsonable_encoder
 
 app = FastAPI()
+client = MongoClient('mongodb://localhost:27017/')
+db = client['courses']
 
 
-# Create ToDoRequest Base Model
-class ToDoRequest(BaseModel):
-    task: str
+"""
+Endpoint to get a list of all available courses. This endpoint needs to support 3 modes of
+sorting: Alphabetical (based on course title, ascending), date (descending) and total course
+rating (descending). Additionaly, this endpoint needs to support optional filtering of courses
+based on domain.
+"""
+@app.get('/courses')
+def get_courses(sort_by: str = 'date', domain: str = None):
+    # set the rating.total and rating.count to all the courses based on the sum of the chapters rating
+    for course in db.courses.find():
+        total = 0
+        count = 0
+        for chapter in course['chapters']:
+            with contextlib.suppress(KeyError):
+                total += chapter['rating']['total']
+                count += chapter['rating']['count']
+        db.courses.update_one({'_id': course['_id']}, {'$set': {'rating': {'total': total, 'count': count}}})
 
 
-@app.get("/")
-def root():
-    return "todooo"
+
+    # sort_by == 'date' [DESCENDING]
+    if sort_by == 'date':
+        sort_field = 'date'
+        sort_order = -1
+
+    # sort_by == 'rating' [DESCENDING]
+    elif sort_by == 'rating':
+        sort_field = 'rating.total'
+        sort_order = -1
+
+    # sort_by == 'alphabetical' [ASCENDING]
+    else:  
+        sort_field = 'name'
+        sort_order = 1
+
+    query = {}
+    if domain:
+        query['domain'] = domain
 
 
-@app.post("/todo", status_code=status.HTTP_201_CREATED)
-def create_todo(todo: ToDoRequest):
-    # create a new database session
-    session = Session(bind=engine, expire_on_commit=False)
+    courses = db.courses.find(query, {'name': 1, 'date': 1, 'description': 1, 'domain':1,'rating':1,'_id': 0}).sort(sort_field, sort_order)
+    return list(courses)
+ 
+ 
+"""
+Endpoint to get the course overview. 
+"""
+@app.get('/courses/{course_id}')
+def get_course(course_id: str):
+    course = db.courses.find_one({'_id': ObjectId(course_id)}, {'_id': 0, 'chapters': 0})
+    if not course:
+        raise HTTPException(status_code=404, detail='Course not found')
+    try:
+        course['rating'] = course['rating']['total']
+    except KeyError:
+        course['rating'] = 'Not rated yet' 
+    
+    return course
+ 
 
-    # create an instance of the ToDo database model
-    tododb = ToDo(task=todo.task)
-
-    # add it to the session and commit it
-    session.add(tododb)
-    session.commit()
-
-    # grab the id given to the object from the database
-    id = tododb.id
-
-    # close the session
-    session.close()
-
-    # return the id
-    return f"created todo item with id {id}"
-
-
-@app.get("/todo/{id}")
-def read_todo(id: int):
-    # create a new database session
-    session = Session(bind=engine, expire_on_commit=False)
-
-    # get the todo item with the given id
-    todo = session.query(ToDo).get(id)
-    # check if todo item with given id exists. If not, raise exception and return 404 not found response
-    if not todo:
-        raise HTTPException(status_code=404, detail=f"todo item with id {id} not found")
-
-    # close the session
-    session.close()
-    return f"todo item with id: {todo.id} and task: {todo.task}"
+""" 
+Endpoint to get a specific chapter information.
+""" 
+@app.get('/courses/{course_id}/{chapter_id}')
+def get_chapter(course_id: str, chapter_id: str):    
+    course = db.courses.find_one({'_id': ObjectId(course_id)}, {'_id': 0, })
+    if not course:
+        raise HTTPException(status_code=404, detail='Course not found')
+    chapters = course.get('chapters', [])
+    try:
+        chapter = chapters[int(chapter_id)]
+    except (ValueError, IndexError) as e:
+        raise HTTPException(status_code=404, detail='Chapter not found') from e
+    return chapter
 
 
-@app.put("/todo/{id}")
-def update_todo(id: int,task : str):
-    # create a new database session
-    session = Session(bind=engine, expire_on_commit=False)
-
-    # get the todo item with the given id
-    todo = session.query(ToDo).get(id)
-
-    # update todo item with the given task (if an item with the given id was found)
-    if todo:
-        todo.task = task
-        session.commit()
-
-    # close the session
-    session.close()
-
-    # check if todo item with given id exists. If not, raise exception and return 404 not found response
-    if not todo:
-        raise HTTPException(status_code=404, detail=f"todo item with id {id} not found")
-
-    return todo
-
-
-@app.delete("/todo/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_todo(id: int):
-
-    # create a new database session
-    session = Session(bind=engine, expire_on_commit=False)
-
-    # get the todo item with the given id
-    todo = session.query(ToDo).get(id)
-
-    # if todo item with given id exists, delete it from the database. Otherwise raise 404 error
-    if todo:
-        session.delete(todo)
-        session.commit()
-        session.close()
-    else:
-        raise HTTPException(status_code=404, detail=f"todo item with id {id} not found")
-
-    return None
-
-@app.get("/todo")
-def read_todo_list():
-    return "read todo list"
-
-
-# Create the database
-Base.metadata.create_all(engine)
+# Endpoint to allow users to rate each chapter (positive/negative) 1 for Positive, -1 For Negative, while aggregating all ratings for each course.
+@app.post('/courses/{course_id}/{chapter_id}')
+def rate_chapter(course_id: str, chapter_id: str, rating: int = Query(..., gt=-2, lt=2)):
+    course = db.courses.find_one({'_id': ObjectId(course_id)}, {'_id': 0, })
+    if not course:
+        raise HTTPException(status_code=404, detail='Course not found')
+    chapters = course.get('chapters', [])
+    try:
+        chapter = chapters[int(chapter_id)]
+    except (ValueError, IndexError) as e:
+        raise HTTPException(status_code=404, detail='Chapter not found') from e
+    try:
+        chapter['rating']['total'] += rating
+        chapter['rating']['count'] += 1
+    except KeyError:
+        chapter['rating'] = {'total': rating, 'count': 1}
+    db.courses.update_one({'_id': ObjectId(course_id)}, {'$set': {'chapters': chapters}})
+    return chapter 
--- readme.txt
+++ readme.txt
@@ -3,3 +3,23 @@ pip install fastapi
 pip install virtualapi
 python -m venv fastapienv
 activate  uvicorn server 
+https://github.com/HighnessAtharva/fastapi-kimo/tree/master
+
+11 feb 2024
+
+pip install fastapi pymongo uvicorn starlette pydantic
+
+pip install pytest 
+
+
+
+
+BSON - Binary serialization format that is used in MongoDB for efficient data storage and retrieval. It comes bundled with PyMongo.
+FastAPI - Web framework for creating Python APIs that offer high performance, automatic validation, interactive documentation, and support for async operations.
+PyMongo -  Official MongoDB driver for Python. It serves as a high-level API for integrating MongoDB within Python.
+Uvicorn - Primary ASGI server that improves application performance. It is responsible for server startup.
+Starlette - ASGI framework that powers FastAPI and allows rapid prototyping development.
+Pydantic - Integrated data validation and parsing library. We need it to create interactive API documentation while automatically validating incoming request data and enforcing data type rules.
+
+
+python -m uvicorn main:app --reload
